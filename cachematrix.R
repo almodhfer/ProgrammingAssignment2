a93a2034052f19c44c4823de6b4ee45e02b07cf9
almodhfer <mokhtarmodhfer@gmail.com>
2015-06-18 12:50:16 +0300

    solving programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## In these two functions we illustrate how we can catch data and save 
+## them to be returned instead of repeatedly re-calculate.
+        
+## this function create a spacial matrix which is actually a list that
+## store getters and stters of the matrix and also getter and setter of the data 
+## we want to catch
+        
 makeCacheMatrix <- function(x = matrix()) {
+        inverse<-NULL
+        set <- function(y)
+        {
+                x<<-y
+                inverse<<-NULL
+        }
+        get<- function() x
+        
+        setinverse<-function(inv) inverse<<-inv
+        getinverse<-function() inverse
+        
+        list(set=set,get=get, setinverse=setinverse,getinverse=getinverse)
+                
+       } 
 
-}
 
-
-## Write a short comment describing this function
+## this function check if we are already calculate the inverse of the matrix x
+## if so we return the inverse, otherwise we calculate it  and set it to the matrix
+## to save it for future use.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse<-x$getinverse()
+        if(!is.null(inverse)){
+                print('we have catched the inverse')
+                return (inverse)
+                }
+         matrix<-x$get()
+         inverse=solve(matrix,...)
+         x$setinverse(inverse)
+         inverse
 }
+
+        
+
